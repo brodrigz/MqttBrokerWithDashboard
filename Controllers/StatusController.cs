4edfb9642ff71bd479fc926e1a1908c5ae178e58
brodrigz <57366583+brodrigz@users.noreply.github.com>
2024-12-27 09:54:13 -0300

    Add example endpoint

---
+++ Controllers/StatusController.cs
@@ -0,0 +1,37 @@
+﻿using MqttBrokerWithDashboard.Extensions;
+using MqttBrokerWithDashboard.Models;
+using MqttBrokerWithDashboard.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MqttBrokerWithDashboard.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class StatusController : ControllerBase
+    {
+        private readonly ILogger<StatusController> logger;
+        private readonly MqttBrokerService mqttBrokerService;
+
+        public StatusController(ILogger<StatusController> logger, MqttBrokerService brokerService)
+        {
+            this.logger = logger;
+            this.mqttBrokerService = brokerService;
+        }
+
+        // GET: api/<ServerStatusController>
+        [HttpGet(nameof(GetConnectedClients))]
+        [ProducesResponseType(StatusCodes.Status200OK, Type = typeof(IEnumerable<MqttClientModel>))]
+        public IEnumerable<MqttClientModel> GetConnectedClients()
+        {
+            return mqttBrokerService.ConnectedClients;
+        }
+    }
+
+}
---
+++ MqttNetExtensions.cs
@@ -0,0 +1,90 @@
+﻿using Microsoft.Extensions.Logging;
+using MQTTnet.Client;
+using MQTTnet;
+using MQTTnet.Diagnostics;
+using Serilog.Core;
+using System;
+using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
+using MQTTnet.Protocol;
+using MQTTnet.Packets;
+using System.Collections.Generic;
+
+namespace MqttBrokerWithDashboard
+{
+    public class MqttNetLogger : IMqttNetLogger
+    {
+        private ILogger _logger;
+        public MqttNetLogger(ILogger logger)
+        {
+            this._logger = logger;
+        }
+
+        public bool IsEnabled { get; set; } = true;
+
+        public void Publish(MqttNetLogLevel logLevel, string source, string message, object[] parameters, Exception exception)
+        {
+            if (!IsEnabled) return;
+
+            // Format the message
+            var formattedMessage = string.Format(message, parameters);
+
+            // Log based on the log level
+            switch (logLevel)
+            {
+                case MqttNetLogLevel.Verbose:
+                    _logger.LogTrace(formattedMessage);
+                    break;
+                case MqttNetLogLevel.Info:
+                    _logger.LogInformation(formattedMessage);
+                    break;
+                case MqttNetLogLevel.Warning:
+                    _logger.LogWarning(formattedMessage);
+                    break;
+                case MqttNetLogLevel.Error:
+                    _logger.LogError(exception, formattedMessage);
+                    break;
+            }
+        }
+    }
+
+    public static class MqttNetExtensions
+    {
+        public static IMqttNetLogger GetMqttNetLogger(this ILogger logger) => new MqttNetLogger(logger);
+
+        /// <summary>
+        /// Creates a MqttClient and connects to the specified broker
+        /// </summary>
+        /// <param name="clientId"></param>
+        /// <param name="host"></param>
+        /// <param name="port"></param>
+        /// <returns>Created client</returns>
+        public static async Task<IMqttClient> CreateAndConnectMqttClient(string clientId, string host, int port, ILogger logger = null)
+        {
+            var factory = new MqttFactory(logger?.GetMqttNetLogger() ?? new MqttNetNullLogger());
+
+            var options = new MqttClientOptionsBuilder()
+                .WithTcpServer(host, port)
+                .WithClientId(clientId)
+                .Build();
+
+            var client = factory.CreateMqttClient();
+
+            await client.ConnectAsync(options);
+
+            return client;
+        }
+
+        public static async Task SubscribeAsync(this IMqttClient client, string topic, MqttQualityOfServiceLevel qos = default)
+        {
+            var filters = new List<MqttTopicFilter> { new MqttTopicFilter { Topic = topic, QualityOfServiceLevel = qos } };
+
+            var subscribeOptions = new MqttClientSubscribeOptions
+            {
+                TopicFilters = filters
+            };
+
+            await client.SubscribeAsync(subscribeOptions);
+        }
+    }
+}
